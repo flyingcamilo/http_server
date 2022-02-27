6c84c3b20ceb4a8126063beae0e3f3527aaebcd0
Camilo Schoeningh <camilo.schoeningh@lhsystems.com>
2022-02-27 20:42:03 +0100

    [STDOUT] Make sure all output is flushed to stdout immediately
    Ruby is buffering output until the output buffer is full.
    To change the behavior we set sync=true
    
    https://ruby-doc.org/core-2.3.1/IO.html#method-i-sync

--- http_server.rb
+++ http_server.rb
@@ -1,13 +1,14 @@
 require 'socket'
 
 server  = TCPServer.new('0.0.0.0', 80)
+$stdout.sync = true
 
 loop {
   client  = server.accept
   request = client.readpartial(2048)
   
   method, path, version = request.lines[0].split
-  puts "#{method} #{path} #{version}"
+  STDOUT.puts "#{method} #{path} #{version}"
 
   if path == "/healthcheck" 
     response = "OK"
