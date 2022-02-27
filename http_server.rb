11e08fe76945284e288a464533fb0fffa19bf4e9
Camilo Schoeningh <camilo.schoeningh@lhsystems.com>
2022-02-27 19:05:32 +0100

    [ADHOC] Use puts instead of write, to append new line

--- http_server.rb
+++ http_server.rb
@@ -7,17 +7,17 @@ loop {
   request = client.readpartial(2048)
   
   method, path, version = request.lines[0].split
-
   puts "#{method} #{path} #{version}"
 
-  headers = "HTTP/1.1 200 OK\r\nDate: #{Time.now}\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n"
-  client.puts headers
-
-  if path == "/healthcheck"
-    client.write("OK")
+  if path == "/healthcheck" 
+    response = "OK"
   else
-    client.write("Well, hello there!")
+    response = "Well, hello there!"
   end
+
+  headers = "HTTP/1.1 200 OK\r\nDate: #{Time.now}\r\nContent-Length: #{response.bytesize}\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n"
+  client.puts headers
+  client.puts response
 
   client.close
 }
