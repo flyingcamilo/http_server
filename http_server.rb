1adfe3f1d06b6957c0d4d2b47eb7a1decdf286c5
Camilo Schoeningh <camilo.schoeningh@lhsystems.com>
2022-02-27 18:33:27 +0100

    [HTTP] Send back response_code and header
    To use the web server in applications like k8s, we should return the
    response code. This can be used inside the readiness and livenessprobe.

--- http_server.rb
+++ http_server.rb
@@ -10,6 +10,9 @@ loop {
 
   puts "#{method} #{path} #{version}"
 
+  headers = "HTTP/1.1 200 OK\r\nDate: #{Time.now}\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n"
+  client.puts headers
+
   if path == "/healthcheck"
     client.write("OK")
   else
